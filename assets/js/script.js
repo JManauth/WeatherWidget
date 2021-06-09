01ca09c9c758ebdb685bba9b30cf55fee463f202
jafet <jafet.manauth@hotmail.com>
2021-06-08 23:01:17 -0500

    fixed https for API, started to display data for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,28 +14,48 @@ if('geolocation' in navigator) {
 
   //API key : 43cd2ebe15da8dcbd0ac1ecba5178ffc
 
-  function currentLocation(){
+function currentLocation(){
+        //gets current location coordinates
         navigator.geolocation.getCurrentPosition(function(position){
             console.log(position);
             var lat = position.coords.latitude;
             var long = position.coords.longitude;
             console.log(lat);
             console.log(long);
+            // finds city with coordinates and displays on website
             var location = document.getElementById('city');
-            var geoAPIURL = 'http://api.openweathermap.org/geo/1.0/reverse?lat=' + lat + '&lon=' + long + '&limit=1&appid=43cd2ebe15da8dcbd0ac1ecba5178ffc';
+            var geoAPIURL = 'https://api.openweathermap.org/geo/1.0/reverse?lat=' + lat + '&lon=' + long + '&limit=1&appid=43cd2ebe15da8dcbd0ac1ecba5178ffc';
             fetch(geoAPIURL)
               .then( Response => Response.json())
               .then( data => $(location).text(data[0].name + ', ' + data[0].state))
               
-              
+            //gets weather for current location
             function currentWeather(lat, long){
                 var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=hourly,minutely&appid=43cd2ebe15da8dcbd0ac1ecba5178ffc';
 
                 fetch(apiURL)
                     .then( res => res.json())
-                    .then( data => console.log(data))
+                    .then( function(data){
+                      console.log(data);
+                      var temp = document.getElementById('temp');
+                      var wind = document.getElementById('wind');
+                      var humidity = document.getElementById('humidity');
+                      var uv = document.getElementById('uv');
+                      //displays weather conditions on screen
+                      $(temp).text('Temp: ' + data.current.temp + '\u00B0' + 'F');
+                      $(wind).text('Wind: ' + data.current.wind_speed + ' MPH');
+                      $(humidity).text('Humidity: ' + data.current.humidity + '%');
+                      $(uv).text('UV index: ' + data.current.uvi);
 
+                      //display weather conditions for 5-day forecast
+                      for( i = 1; i < 6; i++){
+                        var dateDisplay = document.getElementById('date' + i);
+                        temp[i] = document.getElementById('temp' + i);
+                        $(dateDisplay).text(moment().add(i, 'days').format('M D YY'));
+                        $(temp[i]).text('Temp: ' + data.daily[i].temp.day + '\u00B0' + 'F');
+                      }
+                    })
             }
             currentWeather(lat, long);
             });
-    };
+};
