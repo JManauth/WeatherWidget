0ccd6495d5289da4091979e3c8bf3acf1b89137e
jafet <jafet.manauth@hotmail.com>
2021-06-09 20:21:49 -0500

    finished search function code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var humidity = document.getElementById('humidity');
 var uv = document.getElementById('uv');
 var icon = document.getElementById('currentDayIcon');
 var city = document.getElementById('city');
+
 //sets current day and time in the header
 function renderClock(){
     $("#currentDay").text(moment().format("[Today is: ] dddd, MMMM Do YYYY, h:mm a"));
@@ -83,7 +84,7 @@ function currentLocation(){
             currentWeather(lat, long);
             });
 };
-
+//search for city
 $(searchBtn).on('click', function(event){
   event.preventDefault();
   var search = $('#inputCity').val().trim();
@@ -95,18 +96,58 @@ $(searchBtn).on('click', function(event){
     var searchLat = data[0].lat;
     var searchLong = data[0].lon;
     var searchWeatherAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + searchLat + '&lon=' + searchLong + '&units=imperial&exclude=hourly,minutely&appid=43cd2ebe15da8dcbd0ac1ecba5178ffc';
-    
+    $(city).text(data[0].name + ', ' + data[0].state)
+
     fetch(searchWeatherAPI)
     .then(res => res.json())
     .then(function(data){
       console.log(data);
+      var uvValueSearch = data.current.uvi;
       $(icon).attr("src", 'https://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png');
       $(icon).attr("title", data.current.weather[0].description)
       $(temp).text('Temp: ' + data.current.temp + '\u00B0' + 'F');
       $(wind).text('Wind: ' + data.current.wind_speed + ' MPH');
       $(humidity).text('Humidity: ' + data.current.humidity + '%');
       $(uv).text('UV index: ' + data.current.uvi);
+      
+      if( uvValueSearch < 3){
+        $(uv).css('background-color', 'green');
+        $(uv).attr('title', 'Low');
+      } else if ( uvValueSearch < 6){
+        $(uv).css('background-color', 'yellow');
+        $(uv).attr('title', 'Moderate');
+      } else if ( uvValueSearch < 8){
+        $(uv).css('background-color', 'orange');
+        $(uv).attr('title', 'High');
+      } else if ( uvValueSearch < 11){
+        $(uv).css('background-color', 'red');
+        $(uv).attr('title', 'Very High');
+      } else if ( uvValueSearch > 11){
+        $(uv).css('background-color', 'violet');
+        $(uv).attr('title', 'Extreme');
+      }
+
+      //display weather conditions for 5-day forecast
+      for( i = 1; i < 6; i++){
+        var dateDisplay = document.getElementById('date' + i);
+        icon[i] = document.getElementById('icon' + i);
+        temp[i] = document.getElementById('temp' + i);
+        wind[i] = document.getElementById('wind' + i);
+        humidity[i] = document.getElementById('humidity' + i);
+        $(dateDisplay).text(moment().add(i, 'days').format('M/D'));
+        $(icon[i]).attr('src', 'https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png');
+        $(icon[i]).attr("title", data.daily[i].weather[0].description)
+        $(temp[i]).text('Temp: ' + data.daily[i].temp.day + '\u00B0' + 'F');
+        $(wind[i]).text('Wind: ' + data.daily[i].wind_speed + ' MPH');
+        $(humidity[i]).text('Humidity: ' + data.daily[i].humidity + '%');
+      }
     })
   })
 
+});
+
+//save to local storage
+
+$(searchBtn).on('click', function(){
+  console.log('hello');
 })
